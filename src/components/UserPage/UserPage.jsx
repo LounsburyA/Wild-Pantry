6a3659b0a3d955251883a2ab5a071c5bc0c08e27
Andrew Lounsbury <lounsburya@gmail.com>
2022-04-28 13:31:12 -0500

    made small change

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -13,7 +13,7 @@ function UserPage() {
     <>
       <div className="container">
         <h2>Welcome, {user.username}!</h2>
-        <p>Your ID is: {user.id}</p>
+        {/* <p>Your ID is: {user.id}</p> */}
         <LogOutButton className="btn" />
         <div> <button onClick={toPantry} className='btn'>The Pantry</button></div>
       </div>
